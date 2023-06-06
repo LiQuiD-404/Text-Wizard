1117b9b179aa58667804bf59891b2dcf518bc170
Shubham Singh <shubhamsingh.exe@gmail.com>
2023-06-06 22:08:30 +0530

    Updates

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-var fin_val = 0;
-function hello(event){
-    event.preventDefault();
-    var value = document.getElementById('tentacles').value
-    var val = parseInt(value)
-    fin_val = fin_val + val;
-    let ele = document.getElementById('result');
-        ele.innerHTML = parseInt(fin_val);
-}
